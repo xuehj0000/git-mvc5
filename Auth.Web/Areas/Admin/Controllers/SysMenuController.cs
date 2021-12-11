ff4e5a3fd4e9c761fb22537efd89aa1a9151d684
hongjian.xue <15040519070@163.com>
2021-12-12 00:20:39 +0800

    11

---
+++ Auth.Web/Areas/Admin/Controllers/SysMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Auth.Web.Areas.Admin.Controllers
+{
+    public class SysMenuController : Controller
+    {
+        // GET: Admin/SysMenu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Auth.Web/Areas/Admin/Controllers/SysRoleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Auth.Web.Areas.Admin.Controllers
+{
+    public class SysRoleController : Controller
+    {
+        // GET: Admin/SysRole
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
